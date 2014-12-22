ac0b683d386ec684542714ca64d40442e4b91658
Martin Bee <martbee88@gmail.com>
2014-12-21 18:18:30 -0600

    Added Ruby program

--- buddhist_flowchart.rb
+++ buddhist_flowchart.rb
@@ -1,25 +1,19 @@
-buddhist-flowchart
-==================
-
-Simple buddhist flowchart using conditional statements. Asks user if they have a problem, 
-eventually it outputs "Then don't worry!".
-
 =begin
-Conditional version of the Buddhist Flowchart. A loop might be more effective.
+Conditional version of the Buddhist Flowchart. A loop would be more effective.
 Designed to simplify problems and to always display "Then don't worry!" as the end result.
 =end
 
 puts "Do you have a problem in your life, y/n?"
 
-answer = gets.chomp.downcase
+answer = gets.chomp
 
 if answer == "n"
   puts "Then don't worry!"
 elsif answer == "y"
   puts "Can you do something about it, y/n?"
-  answer_2 = gets.chomp.downcase
+  answer_2 = gets.chomp
   if answer_2 == "y" || answer_2 == "n"
-    puts "Then don't worry!"
+  puts"Then don't worry!"
   end
 else
   puts "Please enter y/n."
